bda154fc9df39a896a52a548362e07bdb09a1356
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-11-28 11:02:11 +0100

    Minimal work done

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+
+namespace CityInfo.API {
+    public class CitiesDataStore {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+        public CitiesDataStore() {
+
+            Cities = new List<CityDto>() {
+            new CityDto{
+                Id =1,
+                Name ="Amsterdam",
+                Description = "Weed"
+            },
+            new CityDto{
+                Id =2,
+                Name ="Copenhagen",
+                Description = "Capital of Denmark"
+            },
+            new CityDto {
+                Id=3,
+                Name="Paris",
+                Description="The one with that big tower"
+            }
+            };
+            
+        }
+    }
+}
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -12,6 +12,7 @@ namespace CityInfo.API {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -20,9 +21,11 @@ namespace CityInfo.API {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
+
+            //app.Run(async (context) => {
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
