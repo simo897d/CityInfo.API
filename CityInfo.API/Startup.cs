34475d25e02a4b9ba1449b889565081d22630c2a
Simon Egebjerg Jensen <simo897d@edu.eal.dk>
2018-11-28 12:17:48 +0100

    Nice commit

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -7,13 +7,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace CityInfo.API {
     public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
             // Method below changes the default setting used to output to Json format, specifically the NamingStrategy. 
             //This could be used to change the format we want to output to the user 
                 //.AddJsonOptions(o => {
